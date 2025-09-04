f5c1684dba9235bc6def75f7e97d0a9b59a7ad86
mrfhmd7 <mrf.hmd7@gmail.com>
2025-09-04 23:06:14 +0600

    fided navBar with bg

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <div className='max-w-screen-2xl mx-auto pl-4 pr-4'>
+        <div className='max-w-screen-2xl mx-auto'>
             <RouterProvider router={router} />
         </div>
     </StrictMode>,
--- src/pages/shared/NavBar/NavBar.jsx
+++ src/pages/shared/NavBar/NavBar.jsx
@@ -13,7 +13,7 @@ const NavBar = () => {
     </>
     return (
         <>
-            <div className="navbar bg-gray-600">
+            <div className="navbar fixed z-10 max-w-screen-2xl pl-4 pr-4 bg-opacity-15 bg-gray-600">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
