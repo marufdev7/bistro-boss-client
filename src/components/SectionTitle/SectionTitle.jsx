6fb567b44f1bfc56f6241e74df34977b84c1f2a3
mrfhmd7 <mrf.hmd7@gmail.com>
2025-09-05 23:52:59 +0600

    section title add

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SectionTitle = ({heading, subHeading}) => {
+    return (
+        <div className='flex flex-col items-center justify-center mb-12'>
+            <p className='text-yellow-400 text-lg mb-4 italic'>---{heading}---</p>
+            <h3 className='text-4xl font-medium border-y-4 p-6'>{subHeading}</h3>
+            <hr />
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -12,41 +12,48 @@ import slide2 from '../../../assets/home/slide2.jpg';
 import slide3 from '../../../assets/home/slide3.jpg';
 import slide4 from '../../../assets/home/slide4.jpg';
 import slide5 from '../../../assets/home/slide5.jpg';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
     return (
         <div className='mt-10 mb-16'>
-            <Swiper
-                slidesPerView={4}
-                spaceBetween={30}
-                pagination={{
-                    clickable: true,
-                }}
-                modules={[Pagination]}
-                className="mySwiper"
-            >
-                <SwiperSlide className='flex flex-col'>
-                    <img src={slide1} alt="" />
-                    <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12 '>Salads</h3>
-                </SwiperSlide>
-                <SwiperSlide className='flex flex-col'>
-                    <img src={slide2} alt="" />
-                    <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12 '>Pizzas</h3>
-                </SwiperSlide>
-                <SwiperSlide className='flex flex-col'>
-                    <img src={slide3} alt="" />
-                    <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Soups</h3>
-                </SwiperSlide>
-                <SwiperSlide className='flex flex-col'>
-                    <img src={slide4} alt="" />
-                    <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Desserts</h3>
-                </SwiperSlide>
-                <SwiperSlide className='flex flex-col'>
-                    <img src={slide5} alt="" />
-                    <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Salads</h3>
-                </SwiperSlide>
-            </Swiper>
+            <section>
+                <SectionTitle
+                    heading={"From 11:00 am to 10:00 pm"}
+                    subHeading={"ORDER ONLINE"}
+                />
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide className='flex flex-col'>
+                        <img src={slide1} alt="" />
+                        <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12 '>Salads</h3>
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col'>
+                        <img src={slide2} alt="" />
+                        <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12 '>Pizzas</h3>
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col'>
+                        <img src={slide3} alt="" />
+                        <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Soups</h3>
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col'>
+                        <img src={slide4} alt="" />
+                        <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Desserts</h3>
+                    </SwiperSlide>
+                    <SwiperSlide className='flex flex-col'>
+                        <img src={slide5} alt="" />
+                        <h3 className='text-2xl md:text-4xl uppercase text-center text-gray-200 -mt-12'>Salads</h3>
+                    </SwiperSlide>
+                </Swiper>
+            </section>
         </div>
     );
 };
