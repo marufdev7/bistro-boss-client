d819fb06373cb596f019b768f5ff86609b992c4d
mrfhmd7 <mrf.hmd7@gmail.com>
2025-09-27 12:23:51 +0600

    load menu data

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItems = data.filter(item => item.category === 'popular');
+                setMenu(popularItems);
+        })
+    }, [])
     return (
-        <div className='mt-10'>
+        <section className='mt-10'>
             <SectionTitle
                 heading="From Our Menu"
                 subHeading="Check it Out"
             />
-        </div>
+            <div>
+                {
+                    menu.map(item => {})
+                }
+            </div>
+        </section>
     );
 };
 
