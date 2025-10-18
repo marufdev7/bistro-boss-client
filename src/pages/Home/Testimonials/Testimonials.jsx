54616a112d1809d6de8ceccfa8d0763c73a74595
mrfhmd7 <mrf.hmd7@gmail.com>
2025-10-19 00:48:45 +0600

    swiper navigation add

--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+// import required modules
 
 const Testimonials = () => {
     return (
@@ -8,6 +16,17 @@ const Testimonials = () => {
                 heading={'What Our Clients Say'}
                 subHeading={'Testimonials'}
             />
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                <SwiperSlide>Slide 5</SwiperSlide>
+                <SwiperSlide>Slide 6</SwiperSlide>
+                <SwiperSlide>Slide 7</SwiperSlide>
+                <SwiperSlide>Slide 8</SwiperSlide>
+                <SwiperSlide>Slide 9</SwiperSlide>
+            </Swiper>
         </section>
     );
 };
