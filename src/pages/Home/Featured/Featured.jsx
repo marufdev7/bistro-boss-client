001abb8201c3f75fba8b059fd4a446b5467d9722
mrfhmd7 <mrf.hmd7@gmail.com>
2025-10-19 02:20:23 +0600

    testimonial complete

--- src/pages/Home/Featured/Featured.jsx
+++ src/pages/Home/Featured/Featured.jsx
@@ -10,11 +10,11 @@ const Featured = () => {
         >
             <SectionTitle subHeading="From Our Menu" heading="Check it Out" />
             <div>
-                <div className='md:flex justify-center items-center py-20 px-32'>
+                <div className='md:flex justify-center items-center pb-20 pt-7 px-32'>
                     <div>
                         <img src={featured} alt="" />
                     </div>
-                    <div className='md:ml-10 space-y-3 bg-slate-400 bg-opacity-20'>
+                    <div className='md:ml-10 space-y-3 bg-slate-600 bg-opacity-20'>
                         <p>Sep 29, 2026</p>
                         <p className='uppercase'>Where can i get some?</p>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum ut recusandae aut quae eveniet. Pariatur ab doloribus assumenda aperiam porro, quos explicabo, laboriosam possimus recusandae vero laudantium alias quibusdam velit veritatis. Totam impedit, numquam quis alias qui a tenetur aliquam quidem maiores, suscipit aut excepturi illum ipsam official velit maxime.</p>
--- src/pages/Home/Testimonials/Testimonials.jsx
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
+import { Rating } from '@smastrom/react-rating';
+import { FaQuoteLeft } from 'react-icons/fa';
 
-// Import Swiper styles
+// Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-// import required modules
+// Rating Styles
+import '@smastrom/react-rating/style.css'
+
 
 const Testimonials = () => {
+    const [reviews, setReviews] = useState([]);
+    useEffect(() => {
+        fetch('reviews.json')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [])
     return (
-        <section className=''>
+        <section className='my-20'>
             <SectionTitle
                 heading={'What Our Clients Say'}
                 subHeading={'Testimonials'}
             />
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                {
+                    reviews.map(review => <SwiperSlide
+                        key={review._id}
+                    >
+                        <div className='ml-40 mr-40 w- flex flex-col items-center space-y-5'>
+                            <Rating
+                                style={{ maxWidth: 180 }}
+                                value={review.rating}
+                                readOnly
+                            />
+                            < FaQuoteLeft size={100} />
+                            <p>{review.details}</p>
+                            <h3 className='text-3xl font-semibold text-[#CD9003]'>{review.name}</h3>
+                        </div>
+                    </SwiperSlide>)
+                }
             </Swiper>
         </section>
     );
