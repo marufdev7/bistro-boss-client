9cfc1ba9a0ba45007cac049077ed94ec816d85a0
mrfhmd7 <mrf.hmd7@gmail.com>
2025-10-28 22:16:06 +0600

    cover style fixed

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -6,8 +6,6 @@ import dessertImg from '../../../assets/menu/dessert-bg.jpeg';
 import pizzaImg from '../../../assets/menu/pizza-bg.jpg';
 import saladImg from '../../../assets/menu/salad-bg.jpg';
 import soupImg from '../../../assets/menu/soup-bg.jpg';
-
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
 import useMenu from '../../../hooks/useMenu';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuCategory from '../MenuCategory/MenuCategory';
