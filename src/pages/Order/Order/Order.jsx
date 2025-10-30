dde15e5f939c7edcb9ae3715a8b203106297992b
mrfhmd7 <mrf.hmd7@gmail.com>
2025-10-30 23:57:32 +0600

    menu data and category add in order

--- src/pages/Order/Order/Order.jsx
+++ src/pages/Order/Order/Order.jsx
@@ -3,9 +3,17 @@ import orderCover from '../../../assets/shop/order.jpg'
 import Cover from '../../shared/Cover/Cover';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
+import useMenu from '../../../hooks/useMenu';
 
 const Order = () => {
     const [tabIndex, setTabIndex] = useState(0);
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert');
+    const pizza = menu.filter(item => item.category === 'pizza');
+    const salad = menu.filter(item => item.category === 'salad');
+    const soup = menu.filter(item => item.category === 'soup');
+    const offered = menu.filter(item => item.category === 'offered');
+
     return (
         <div>
             <Cover
